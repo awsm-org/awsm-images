9beddffb06d0ddd0138fc1f2aa8256236a5503a0
doapp-ryanp <ryanp@doapps.com>
2015-09-30 09:31:13 -0500

    code in awsm dir cant depend on mods defined in awsm package.json

--- awsm/thumbnail/index.js
+++ awsm/thumbnail/index.js
@@ -1,14 +1,27 @@
 'use strict';
 
-var AWS     = require('aws-sdk'),
-    Promise = require('bluebird'),
-    s3      = Promise.promisifyAll(new AWS.S3()),
-    rp      = require('request-promise'),
-    shortid = require('shortid');
+/**
+ * !!!!!!!!!!!!!IMPORTANT!!!!!!!!!!!
+ *
+ * Code in awsm dir of an aws module is just skeletion/example code and CAN be modified by user who installed awsm.
+ *
+ * Code inside an aws module's 'awsm' dir is copied to project's aws_modules/<mod name> folder (during npm postinstall)
+ *
+ * Therefore, npm 'dependencies' defined in the module you are creating CAN NOT be used here as node require loader wont
+ * be able to find it.
+ *
+ * For example if awsm-images had a 'dependency' of bluebird defined in its package.json, bluebird could NOT be used
+ * in this file.  This file will live in <project>/aws_modules/awsm-images/thumbnail/index.js and the bluebird mod will
+ * be included in <project>/node_modules/awsm-images/node_modules/bluebird.
+ *
+ * In npm v3 dependencies were flattened (https://github.com/npm/npm/releases/tag/v3.0.0) HOWEVER you can still NOT
+ * rely on skeletion/example having access to a package the module you are creating depends on because there could be
+ * conflicting dependencies (and then your dependency wont be on the flattened path).
+ *
+ * Keep in mind awsm nodejs mods are just npm modules and should be treated as such.
+ */
 
 var ir = require('awsm-images/lib/resize.js');
-
-console.log('Using AWS SDK ver: ' + require('aws-sdk/package.json').version);
 
 /**
  * Export for lambda handler
@@ -25,25 +38,8 @@ exports.run = function(event, context, cb) {
     return cb(new Error('IMAGE_RESIZE_BUCKET env var not set'));
   }
 
-  var imgBucket = process.env.IMAGE_RESIZE_BUCKET,
-      key       = [process.env.JAWS_DATA_MODEL_STAGE, shortid.generate() + '.jpg'].join('/');
-
   ir.thumbnail(event.url)
-      .then(function(thumbBuffer) {
-        //upload thumb to s3
-        var params = {
-          Bucket: imgBucket,
-          Key: key,
-          ACL: 'public-read',
-          Body: thumbBuffer,
-          ContentType: 'image/jpeg'
-        };
-
-        return s3.putObjectAsync(params);
-      })
-      .then(function() {
-        var s3HttpsUrl = 'https://' + s3.endpoint.hostname + '/' + imgBucket + '/' + key;
-
+      .then(function(s3HttpsUrl) {
         return cb(null, {thumbUrl: s3HttpsUrl});
       })
       .catch(function(e) {
